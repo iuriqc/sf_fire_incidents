ed04b4f7146720761f8d601cc894667aa6f46de8
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 16:18:09 -0300

    Fix: Include iam module reference and bucket name into main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,13 +21,20 @@ module "storage" {
   environment = var.environment
 }
 
+module "iam" {
+  source      = "./modules/iam"
+  environment = var.environment
+}
+
 module "processing" {
   source         = "./modules/processing"
   environment    = var.environment
   glue_role_arn  = module.iam.glue_role_arn
   s3_bucket_name = module.storage.data_bucket_name
+  subnet_id      = module.networking.private_subnet_ids[0]
+  vpc_id         = module.networking.vpc_id
 
-  depends_on = [module.storage]
+  depends_on = [module.storage, module.iam]
 }
 
 module "warehouse" {
@@ -36,6 +43,7 @@ module "warehouse" {
   vpc_id            = module.networking.vpc_id
   subnet_ids        = module.networking.private_subnet_ids
   redshift_password = var.redshift_password
+  s3_bucket_name    = module.storage.data_bucket_name 
 
   depends_on = [module.networking]
 }
