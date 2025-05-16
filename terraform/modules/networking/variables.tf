fc5eab58538790c573e98508656f8b0473656265
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 11:01:25 -0300

    Feat: Add networking variables

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "Deployment environment (dev/hml/prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
