528f3ce3c6d16e32fe0bad4005a78c5f4fc7c90d
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 11:35:03 -0300

    Feat: Add validation to processing variables

--- terraform/modules/processing/variables.tf
+++ terraform/modules/processing/variables.tf
@@ -1,6 +1,11 @@
 variable "environment" {
   description = "Deployment environment prefix"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "hml", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, hml, prod"
+  }
 }
 
 variable "glue_role_arn" {
