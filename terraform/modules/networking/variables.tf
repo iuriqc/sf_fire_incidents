455d22f92060860cbf9d6ecfc6fea626051ec485
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 11:02:40 -0300

    Feat: Update networking variables

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,6 +1,12 @@
 variable "environment" {
   description = "Deployment environment (dev/hml/prod)"
   type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "hml", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, hml, prod"
+  }
 }
 
 variable "vpc_cidr" {
