a7bf3066b9b664d81cfc0f67e647702f338edc90
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 11:31:56 -0300

    Feat: Add processing main

---
+++ terraform/modules/processing/main.tf
@@ -0,0 +1,43 @@
+resource "aws_glue_crawler" "fire_incidents" {
+  name          = "sf-fire-${var.environment}-crawler"
+  role          = var.glue_role_arn
+  database_name = "fire_incidents"
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/raw/"
+  }
+
+  configuration = jsonencode({
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+    }
+    Version = 1
+  })
+}
+
+resource "aws_emr_cluster" "fire_etl" {
+  name          = "sf-fire-${var.environment}-etl"
+  release_label = "emr-6.4.0"
+  applications  = ["Spark"]
+  log_uri       = "s3://${var.s3_bucket_name}/logs/"
+
+  ec2_attributes {
+    subnet_id                         = var.subnet_ids[0]
+    emr_managed_master_security_group = aws_security_group.emr.id
+    emr_managed_slave_security_group = aws_security_group.emr.id
+    instance_profile                  = aws_iam_instance_profile.emr.arn
+  }
+
+  master_instance_group {
+    instance_type = "m5.xlarge"
+  }
+
+  core_instance_group {
+    instance_type  = "m5.xlarge"
+    instance_count = 1
+  }
+
+  auto_termination_policy {
+    idle_timeout = 3600
+  }
+}
