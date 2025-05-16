38a1a89fbe1c0f14926eec7da5b2cc1ffcc16610
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-16 16:03:57 -0300

    Feat: Update warehouse variables

--- terraform/modules/warehouse/variables.tf
+++ terraform/modules/warehouse/variables.tf
@@ -1,6 +1,11 @@
 variable "environment" {
   description = "Deployment environment prefix"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "hml", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, hml, prod"
+  }
 }
 
 variable "vpc_id" {
@@ -29,4 +34,14 @@ variable "redshift_nodes" {
   description = "Number of Redshift nodes"
   type        = number
   default     = 1
+
+  validation {
+    condition     = var.redshift_nodes >= 1 && var.redshift_nodes <= 8
+    error_message = "Number of nodes must be between 1 and 8"
+  }
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name for Athena query results"
+  type        = string
 }
