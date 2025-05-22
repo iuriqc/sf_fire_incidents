d1d455cb7446e2b281b112ec1d52142982e17a9d
Iuri Queiroz <iuriqc@hotmail.com>
2025-05-22 16:37:30 -0300

    Fix: Remove unnecessary step

--- dags/sf_fire_incidents_etl.py
+++ dags/sf_fire_incidents_etl.py
@@ -3,10 +3,8 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.dbt import DbtRunOperator, DbtTestOperator
-import sys
-import os
+import boto3
 import logging
 
 logger = logging.getLogger(__name__)
@@ -23,45 +21,21 @@ default_args = {
     'execution_timeout': timedelta(hours=1),
 }
 
-def download_scripts(**context):
-    """Download scripts from S3 to Airflow worker"""
-    s3 = S3Hook(aws_conn_id='aws_default')
-    scripts_path = '/opt/airflow/scripts'
-    os.makedirs(scripts_path, exist_ok=True)
-    
-    s3.download_file(
-        key='scripts/sf_fire_extract.py',
-        bucket_name='your-s3-bucket',
-        local_path=f"{scripts_path}/sf_fire_extract.py"
-    )
-    
-    return scripts_path
-
 def run_extract_script(**context):
     """Execute the extraction script"""
-    scripts_path = context['task_instance'].xcom_pull(task_ids='download_scripts')
-    sys.path.append(scripts_path)
+    s3 = boto3.client('s3')
+    bucket_name = context['var'].get('s3_bucket')
     
-    from sf_fire_extract import main
     try:
-        main()
+        response = s3.get_object(
+            Bucket=bucket_name,
+            Key='scripts/sf_fire_extract.py'
+        )
+        exec(response['Body'].read().decode())
+
     except Exception as e:
         logger.error(f"Extract script failed: {str(e)}")
         raise
-
-def prepare_dbt_project(**context):
-    """Download and prepare dbt project"""
-    s3 = S3Hook(aws_conn_id='aws_default')
-    dbt_path = '/opt/airflow/dbt'
-    os.makedirs(dbt_path, exist_ok=True)
-    
-    s3.download_directory(
-        key='dbt/',
-        bucket_name='your-s3-bucket',
-        local_path=dbt_path
-    )
-    
-    return dbt_path
 
 # Create DAG
 with DAG(
@@ -76,12 +50,6 @@ with DAG(
 
     start = DummyOperator(task_id='start')
     end = DummyOperator(task_id='end')
-    
-    download_scripts_task = PythonOperator(
-        task_id='download_scripts',
-        python_callable=download_scripts,
-        provide_context=True
-    )
 
     extract_task = PythonOperator(
         task_id='extract_and_load',
@@ -89,28 +57,24 @@ with DAG(
         provide_context=True
     )
 
-    prepare_dbt = PythonOperator(
-        task_id='prepare_dbt',
-        python_callable=prepare_dbt_project,
-        provide_context=True
-    )
-
     dbt_run = DbtRunOperator(
         task_id='dbt_run',
-        dbt_root_path='{{ task_instance.xcom_pull(task_ids="prepare_dbt") }}',
+        dbt_root_path=f"s3://{{'{{ var.value.s3_bucket }}'}}/dbt",
         select=['models'],
+        full_refresh=True,
         env={
-            'DBT_PROFILES_DIR': '{{ task_instance.xcom_pull(task_ids="prepare_dbt") }}'
+            'DBT_PROFILES_DIR': f"s3://{{'{{ var.value.s3_bucket }}'}}/dbt"
         }
     )
 
     dbt_test = DbtTestOperator(
         task_id='dbt_test',
-        dbt_root_path='{{ task_instance.xcom_pull(task_ids="prepare_dbt") }}',
-        select=['models'],
+        dbt_root_path=f"s3://{{'{{ var.value.s3_bucket }}'}}/dbt",
+        select=['tests'],
+        retries=1,
         env={
-            'DBT_PROFILES_DIR': '{{ task_instance.xcom_pull(task_ids="prepare_dbt") }}'
+            'DBT_PROFILES_DIR': f"s3://{{'{{ var.value.s3_bucket }}'}}/dbt"
         }
     )
 
-    start >> download_scripts_task >> extract_task >> prepare_dbt >> dbt_run >> dbt_test >> end
+    start >> extract_task >> dbt_run >> dbt_test >> end
